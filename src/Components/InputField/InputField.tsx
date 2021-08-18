55ec0b187726f718f42c4eb1ddfab479c4febeff
programmergeek <programergeek@gmail.com>
2021-08-18 12:57:25 +0200

    added state and state handlers

--- src/Components/InputField/InputField.tsx
+++ src/Components/InputField/InputField.tsx
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { ColorPicker } from "react-pick-color";
 
 export const InputField: React.FC = () => {
+  const [colour, setColour] = useState("");
+
+  const handleColourChange = (colour: string) => {
+    if (colour[0] === "#") {
+      setColour(colour);
+    } else {
+      setColour(`#${colour}`);
+    }
+  };
+
   return (
     <div className="field-container">
-      <input className="input-field" />
+      <input
+        className="input-field"
+        value={colour.toUpperCase()}
+        onChange={(e) => handleColourChange(e.target.value)}
+      />
       <div className="colour-picker">
-        <ColorPicker />
+        <ColorPicker
+          color={colour.toUpperCase()}
+          onChange={(colour) => handleColourChange(colour.hex.toUpperCase())}
+        />
       </div>
     </div>
   );
