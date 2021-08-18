b1411f710ae27a8fb95a58eb3634db7ea981ed24
programmergeek <programergeek@gmail.com>
2021-08-18 12:39:27 +0200

    created input field and colour picker

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import { InputField } from "./Components";
 
 const App: React.FC = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <InputField />
+    </div>
+  );
 };
 
 export default App;
--- src/Components/InputField/InputField.tsx
+++ src/Components/InputField/InputField.tsx
@@ -1,5 +1,13 @@
 import React from "react";
+import { ColorPicker } from "react-pick-color";
 
 export const InputField: React.FC = () => {
-  return <div></div>;
+  return (
+    <div className="field-container">
+      <input className="input-field" />
+      <div className="colour-picker">
+        <ColorPicker />
+      </div>
+    </div>
+  );
 };
