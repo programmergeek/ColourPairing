b6b0a81436d92ab26422e7604af27a4ff5cfe2b6
programmergeek <programergeek@gmail.com>
2021-08-21 23:10:31 +0200

    created code output template

--- src/Components/CodeBlock/CodeBlock.tsx
+++ src/Components/CodeBlock/CodeBlock.tsx
@@ -21,8 +21,8 @@ interface Props {
       text: string;
     };
     clipArt?: {
-      firstCircle: string;
-      secondCircle: string;
+      primary: string;
+      secondary?: string;
     };
   };
 }
@@ -31,7 +31,32 @@ export const CodeBlock: React.FC<Props> = ({ ...props }: Props) => {
   const stylesCSS = `
     --background-colour: ${props.colours.background};
     --text-colour: ${props.colours.text};
-    --primary-button-background-colour: ${props.colours.primaryButton?.background}
+    --primary-button-background-colour: ${props.colours.primaryButton?.background};
+    --primary-button-border-colour: ${props.colours.primaryButton?.border};
+    --primary-button-text-colour: ${props.colours.primaryButton?.text};
+    --secondary-button-background-colour: ${props.colours.secondaryButton?.background};
+    --secondary-button-border-colour: ${props.colours.secondaryButton?.border};
+    --secondary-button-text-colour: ${props.colours.secondaryButton?.text};
+    --tertiary-button-background-colour: ${props.colours.tertiaryButton?.background};
+    --tertiary-button-border-colour: ${props.colours.tertiaryButton?.border};
+    --tertiary-button-text-colour: ${props.colours.tertiaryButton?.text};
+    --art-primary-colour: ${props.colours.clipArt?.primary};
+    --art-secondary-colour: ${props.colours.clipArt?.secondary};
+    `;
+  const stylesSCSS = `
+    $background-colour: ${props.colours.background};
+    $text-colour: ${props.colours.text};
+    $primary-button-background-colour: ${props.colours.primaryButton?.background};
+    $primary-button-border-colour: ${props.colours.primaryButton?.border};
+    $primary-button-text-colour: ${props.colours.primaryButton?.text};
+    $secondary-button-background-colour: ${props.colours.secondaryButton?.background};
+    $secondary-button-border-colour: ${props.colours.secondaryButton?.border};
+    $secondary-button-text-colour: ${props.colours.secondaryButton?.text};
+    $tertiary-button-background-colour: ${props.colours.tertiaryButton?.background};
+    $tertiary-button-border-colour: ${props.colours.tertiaryButton?.border};
+    $tertiary-button-text-colour: ${props.colours.tertiaryButton?.text};
+    $art-primary-colour: ${props.colours.clipArt?.primary};
+    $art-secondary-colour: ${props.colours.clipArt?.secondary};
     `;
 
   return <div className="code-block-container"></div>;
