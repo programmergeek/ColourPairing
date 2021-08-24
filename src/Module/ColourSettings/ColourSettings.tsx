87a5a628c75976868ccbd68e920792f8b8a4a559
programmergeek <programergeek@gmail.com>
2021-08-24 11:50:06 +0200

    added remaining elements

--- src/Module/ColourSettings/ColourSettings.tsx
+++ src/Module/ColourSettings/ColourSettings.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { InputField } from "../../Components";
+import { CodeBlock, DropDownMenu, InputField } from "../../Components";
 import "./styles.css";
 
 export const ColourSettings: React.FC = () => {
   return (
     <div className="colour-settings">
-      <h1 className="header">Colour Settings</h1>
+      <h1 className="header-cs">Colour Settings</h1>
       <div id="background-and-text">
         <div id="background">
           <label className="label"> Background Colour</label>
@@ -15,6 +15,35 @@ export const ColourSettings: React.FC = () => {
           <label className="label"> Text Colour</label>
           <InputField />
         </div>
+      </div>
+      <div className="drop-menus">
+        <div className="menu">
+          <DropDownMenu
+            fields={["Background Colour", "Border Colour", "Text Colour"]}
+            header="Primary Button"
+          />
+        </div>
+        <div className="menu">
+          <DropDownMenu
+            fields={["Background Colour", "Border Colour", "Text Colour"]}
+            header="Secondary Button"
+          />
+        </div>
+        <div className="menu">
+          <DropDownMenu
+            fields={["Background Colour", "Border Colour", "Text Colour"]}
+            header="Tertiary Button"
+          />
+        </div>
+        <div className="menu">
+          <DropDownMenu
+            fields={["Primary Colour", "Secondary Colour"]}
+            header="Art"
+          />
+        </div>
+      </div>
+      <div className="code-block-cs">
+        <CodeBlock colours={{ background: "#000", text: "#fff" }} />
       </div>
     </div>
   );
